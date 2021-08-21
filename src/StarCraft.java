8cfb7eb3297b9ac81abdbbe4c24bca7268bca4fa
hwj <jminie@naver.com>
2021-08-21 11:50:46 +0900

    commit-4

--- src/StarCraft.java
+++ src/StarCraft.java
@@ -1,32 +1,45 @@
 public class StarCraft {
+
+    public static void main(String[] args) {
+        // 객체 생성
+        marine marine = new marine("레이너",100);
+        medic medic = new medic("모랄레스",50);
+        // 마린의 스팀팩!
+        marine.stimpack();
+        // 메딕의 힐!
+        medic.heal(marine);
+    }
+}
+class marine{
     String name;
     int hp;
 
-    public StarCraft(String name, int hp) {
+    public marine(String name, int hp) {
         this.name = name;
         this.hp = hp;
     }
 
     void stimpack(){
-        System.out.printf("[%s]의 스팀팩! HP: %d -> ", name, hp);
+        System.out.printf("[%s]의 스팀팩! HP: %d -> ", this.name, this.hp);
         hp -= 10;
-        System.out.printf("%d\n", hp);
+        System.out.printf("%d\n", this.hp);
     }
 
-    void heal(StarCraft hero){
-        System.out.printf("[%s]의 치유! => [%s] HP(%d -> ",this.name , name, hp );
+}
+
+class medic{
+    String name;
+    int hp;
+
+    public medic(String name, int hp) {
+        this.name = name;
+        this.hp = hp;
+    }
+
+    void heal(marine hero){
+        System.out.printf("[%s]의 치유! => [%s] HP(%d -> ",this.name , hero.name, hero.hp );
         hero.hp += 10;
-        System.out.printf("%d)",hp);
-    }
-
-    public static void main(String[] args) {
-        // 객체 생성
-        StarCraft marine = new StarCraft("레이너",80);
-        StarCraft medic = new StarCraft("모랄레스",60);
-        // 마린의 스팀팩!
-        marine.stimpack();
-        // 메딕의 힐!
-        medic.heal(marine);
+        System.out.printf("%d)",hero.hp);
     }
 
 }
