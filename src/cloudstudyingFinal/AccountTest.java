f3299ef9847ff518dd3b2fd37300f75a3859ce5a
hwj <jminie@naver.com>
2021-09-23 21:06:25 +0900

    연습문제 D - 7

---
+++ src/cloudstudyingFinal/AccountTest.java
@@ -0,0 +1,43 @@
+package cloudstudyingFinal;
+
+public class AccountTest {
+    public static void main(String[] args) {
+        // 객체 생성
+        Account a = new Account("123-45", 10000);
+        Account b = new Account("567-89", 10000);
+
+        // 송금: 3천원씩 a 계좌에서 -> b계좌로!
+        boolean result = true;
+        while (result) {
+            result = a.transfer(b, 3000);
+        }
+
+        // 결과 출력
+        System.out.println(a.toString());
+        System.out.println(b.toString());
+    }
+}
+
+class Account {
+    String num;  // 계좌번호
+    int balance; // 잔액
+
+    public Account(String num, int balance) {
+        this.num = num;
+        this.balance = balance;
+    }
+
+    public String toString() {
+        return String.format("Account { num: %s, balance: %d }", num, balance);
+    }
+
+    public boolean transfer(Account target, int amount) {
+        if (this.balance - 3000 < 0) {
+            return false;
+        }
+        /* 2. 현재 잔액에서 송금액을 이체대상 계좌로 전달하세요. */
+        target.balance += 3000;
+        this.balance -= 3000;
+        return true;
+    }
+}
