a961ba0ca48a6574dae665e0589b44a0c8784883
hwj <jminie@naver.com>
2021-09-25 17:38:30 +0900

    연습문제 D - 9 (에러사항있음)

---
+++ src/cloudstudyingFinal/Lotto.java
@@ -0,0 +1,44 @@
+package cloudstudyingFinal;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Lotto {
+    public static void main(String[] args) {
+        RandomNumGenerate randomNum = new RandomNumGenerate();
+        makeString makeString = new makeString();
+
+        List<Integer> lotto = randomNum.randomMake();
+        System.out.println(makeString.toString(lotto));
+
+    }
+}
+
+class RandomNumGenerate{
+    Random random = new Random();
+    public List<Integer> randomMake(){
+        List<Integer> lottoNum = new ArrayList<>();
+
+        for(int i = 0; i < 6; i++){
+            int number = random.nextInt(45) + 1;
+            if(!lottoNum.contains(number)){
+                lottoNum.add(number);
+            }
+        }
+        return lottoNum;
+    }
+}
+
+class makeString{
+    public String toString(List<Integer> lottoNum) {
+        String result = "";
+        String a = "";
+        for (int i = 0; i < 6; i++) {
+            a += String.format("%d ", lottoNum.get(i));
+        }
+        result = "생성 번호: " + a;
+        return result;
+    }
+}
+
