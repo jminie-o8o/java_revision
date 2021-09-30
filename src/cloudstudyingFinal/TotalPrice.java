5ced24552037d2ee4e425f088f34e5a5f284313e
hwj <jminie@naver.com>
2021-09-30 17:19:45 +0900

    연습문제 D - 11

---
+++ src/cloudstudyingFinal/TotalPrice.java
@@ -0,0 +1,55 @@
+package cloudstudyingFinal;
+
+import java.util.ArrayList;
+
+public class TotalPrice {
+    public static void main(String[] args) {
+        /* 1. 구매 상품 객체를 생성하세요. */
+        Item item1 = new Item("셔츠",49900);
+        Item item2 = new Item("바지",58900);
+        Item item3 = new Item("양말",46900);
+        /* 2. 장바구니에 상품을 담으세요. */
+        Cart myCart = new Cart();
+        myCart.add(item1);
+        myCart.add(item2);
+        myCart.add(item3);
+
+        // 총합 출력
+        System.out.printf("총합: %d원", myCart.totalPrice());
+    }
+}
+
+class Cart {
+    private ArrayList<Item> list;
+
+    public Cart() {
+        this.list = new ArrayList<Item>();
+    }
+
+    public void add(Item item) {
+        list.add(item);
+    }
+
+    /* 3. 장바구니 총합 메소드를 완성하세요. */
+    public int totalPrice() {
+        int sum = 0;
+        for(int i = 0; i < list.size(); i++){
+            sum += list.get(i).getPrice();
+        }
+        return sum;
+    }
+}
+
+class Item {
+    private String name;
+    private int price;
+
+    public Item(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
