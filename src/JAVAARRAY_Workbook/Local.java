7e7575751a44d0b56cd0022fb830448b843c6374
hwj <jminie@naver.com>
2021-10-25 21:35:15 +0900

    연습문제 C - 반복문과 배열 2번 리팩토링

--- src/JAVAARRAY_Workbook/Local.java
+++ src/JAVAARRAY_Workbook/Local.java
@@ -1,13 +1,34 @@
 package JAVAARRAY_Workbook;
 
+import java.util.ArrayList;
+
 public class Local {
     public static void main(String[] args) {
         // 배열 생성
-        String[] cities = {"서울", "부산", "인천", "대전", "대구"};
-        int[] visitors = {599, 51, 46, 43, 27};
+        City Seoul = new City("서울" ,599);
+        City Busan = new City("부산" ,51);
+        City Incheon = new City("인천" ,46);
+        City Daejeon = new City("대전" ,43);
+        City Daegu = new City("대구" ,27);
 
-        for (int i = 0; i < cities.length; i++) {
-            System.out.printf("%s: %d 명\n", cities[i], visitors[i]);
+        ArrayList<City> cities = new ArrayList<>();
+        cities.add(Seoul);
+        cities.add(Busan);
+        cities.add(Incheon);
+        cities.add(Daejeon);
+        cities.add(Daegu);
+        for (int i = 0; i < cities.size(); i++) {
+            System.out.printf("%s: %d 명\n", cities.get(i).city, cities.get(i).visitors);
         }
+    }
+}
+
+class City{
+    String city;
+    int visitors;
+
+    public City(String city, int visitors) {
+        this.city = city;
+        this.visitors = visitors;
     }
 }
